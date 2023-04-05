61f9fa8e8ebbd4c66da6f227201eca3345d6cea6
domire8 <domire8@users.noreply.github.com>
2023-04-05 06:17:16 +0000

    deploy: 7e00bceeaab21473731f2c73ae28175b8b14b8f5

--- versions/develop/search/classes_7.js
+++ versions/develop/search/classes_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['subscriptionhandler_0',['SubscriptionHandler',['../classmodulo__core_1_1communication_1_1_subscription_handler.html',1,'modulo_core::communication']]],
-  ['subscriptioninterface_1',['SubscriptionInterface',['../classmodulo__core_1_1communication_1_1_subscription_interface.html',1,'modulo_core::communication']]]
+  ['serviceclient_0',['ServiceClient',['../classmodulo__utils_1_1testutils_1_1_service_client.html',1,'modulo_utils::testutils']]],
+  ['subscriptionhandler_1',['SubscriptionHandler',['../classmodulo__core_1_1communication_1_1_subscription_handler.html',1,'modulo_core::communication']]],
+  ['subscriptioninterface_2',['SubscriptionInterface',['../classmodulo__core_1_1communication_1_1_subscription_interface.html',1,'modulo_core::communication']]]
 ];
