218839a70ce3a565b143905cd38a53f9ada98732
Dominic Reber <dominic@aica.tech>
2023-10-19 10:07:25 +0000

    feat: add utility function to modify parameter overrides

--- source/modulo_components/modulo_components/utilities/utilities.py
+++ source/modulo_components/modulo_components/utilities/utilities.py
@@ -1,4 +1,7 @@
 import re
+from typing import List
+
+from rclpy import Parameter
 
 
 def parse_topic_name(topic_name: str) -> str:
@@ -12,3 +15,37 @@ def parse_topic_name(topic_name: str) -> str:
     sanitized_string = re.sub("\W", "", topic_name, flags=re.ASCII).lower()
     sanitized_string = sanitized_string.lstrip("_")
     return sanitized_string
+
+
+def modify_parameter_overrides(parameter_overrides: List[Parameter]) -> List[Parameter]:
+    """
+    Modify parameter overrides to handle the rate and period parameters.
+    This function checks for existence of the rate and period parameter in the parameter overrides and modifies them to
+    correspond to the same value. If both the rate and the period parameter exist, the period will be set from the rate.
+
+    :param parameter_overrides: The parameter overrides passed to the component constructor
+    :return: The modified parameter overrides 
+    """
+    rate = None
+    period = None
+    parameters = []
+    for parameter in parameter_overrides:
+        if parameter.name == "rate":
+            rate = parameter
+        elif parameter.name == "period":
+            period = parameter
+        else:
+            parameters.append(parameter)
+    
+    if rate is not None and period is not None:
+        rate = Parameter("rate", value=rate.get_parameter_value().integer_value)
+        period = Parameter("period", value=1.0 / rate.get_parameter_value().integer_value)
+    elif period is not None:
+        period = Parameter("period", value=period.get_parameter_value().double_value)
+        rate = Parameter("rate", value=int(1.0 / period.get_parameter_value().double_value))
+    else:
+        return parameters
+
+    parameters.append(rate)
+    parameters.append(period)
+    return parameters
