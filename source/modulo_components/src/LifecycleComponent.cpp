3b3294c3dbb6c1899e3f454cf5bad7350a1bd47b
Dominic Reber <71256590+domire8@users.noreply.github.com>
2024-10-03 12:50:33 +0200

    feat: optimize creation and configuration of lifecycle publishers (#149)

--- source/modulo_components/src/LifecycleComponent.cpp
+++ source/modulo_components/src/LifecycleComponent.cpp
@@ -243,7 +243,60 @@ bool LifecycleComponent::configure_outputs() {
       auto topic_name = this->get_parameter_value<std::string>(name + "_topic");
       RCLCPP_DEBUG_STREAM(
           this->get_logger(), "Configuring output '" << name << "' with topic name '" << topic_name << "'.");
-      interface = this->output_configuration_callables_.at(name)(topic_name);
+      auto message_pair = interface->get_message_pair();
+      switch (message_pair->get_type()) {
+        case MessageType::BOOL: {
+          auto publisher = this->create_publisher<std_msgs::msg::Bool>(topic_name, this->get_qos());
+          interface = std::make_shared<PublisherHandler<LifecyclePublisher<std_msgs::msg::Bool>, std_msgs::msg::Bool>>(
+                          PublisherType::LIFECYCLE_PUBLISHER, publisher)
+                          ->create_publisher_interface(message_pair);
+          break;
+        }
+        case MessageType::FLOAT64: {
+          auto publisher = this->create_publisher<std_msgs::msg::Float64>(topic_name, this->get_qos());
+          interface =
+              std::make_shared<PublisherHandler<LifecyclePublisher<std_msgs::msg::Float64>, std_msgs::msg::Float64>>(
+                  PublisherType::LIFECYCLE_PUBLISHER, publisher)
+                  ->create_publisher_interface(message_pair);
+          break;
+        }
+        case MessageType::FLOAT64_MULTI_ARRAY: {
+          auto publisher = this->create_publisher<std_msgs::msg::Float64MultiArray>(topic_name, this->get_qos());
+          interface = std::make_shared<PublisherHandler<
+              LifecyclePublisher<std_msgs::msg::Float64MultiArray>, std_msgs::msg::Float64MultiArray>>(
+                          PublisherType::LIFECYCLE_PUBLISHER, publisher)
+                          ->create_publisher_interface(message_pair);
+          break;
+        }
+        case MessageType::INT32: {
+          auto publisher = this->create_publisher<std_msgs::msg::Int32>(topic_name, this->get_qos());
+          interface =
+              std::make_shared<PublisherHandler<LifecyclePublisher<std_msgs::msg::Int32>, std_msgs::msg::Int32>>(
+                  PublisherType::LIFECYCLE_PUBLISHER, publisher)
+                  ->create_publisher_interface(message_pair);
+          break;
+        }
+        case MessageType::STRING: {
+          auto publisher = this->create_publisher<std_msgs::msg::String>(topic_name, this->get_qos());
+          interface =
+              std::make_shared<PublisherHandler<LifecyclePublisher<std_msgs::msg::String>, std_msgs::msg::String>>(
+                  PublisherType::LIFECYCLE_PUBLISHER, publisher)
+                  ->create_publisher_interface(message_pair);
+          break;
+        }
+        case MessageType::ENCODED_STATE: {
+          auto publisher = this->create_publisher<modulo_core::EncodedState>(topic_name, this->get_qos());
+          interface = std::make_shared<
+                          PublisherHandler<LifecyclePublisher<modulo_core::EncodedState>, modulo_core::EncodedState>>(
+                          PublisherType::LIFECYCLE_PUBLISHER, publisher)
+                          ->create_publisher_interface(message_pair);
+          break;
+        }
+        case MessageType::CUSTOM_MESSAGE: {
+          interface = this->custom_output_configuration_callables_.at(name)(topic_name);
+          break;
+        }
+      }
     } catch (const modulo_core::exceptions::CoreException& ex) {
       success = false;
       RCLCPP_ERROR_STREAM(this->get_logger(), "Failed to configure output '" << name << "': " << ex.what());
