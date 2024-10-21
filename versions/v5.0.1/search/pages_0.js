6dc59767457f01887be4690a06c2fe7b3af04ba7
domire8 <domire8@users.noreply.github.com>
2024-10-21 07:57:07 +0000

    deploy: 731e8e278d26500b278ad891a99d028d697e2396

---
+++ versions/v5.0.1/search/pages_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['components_0',['Modulo Components',['../md__2github_2workspace_2source_2modulo__components_2_r_e_a_d_m_e.html',1,'']]],
+  ['controllers_1',['modulo-controllers',['../md__2github_2workspace_2source_2modulo__controllers_2_r_e_a_d_m_e.html',1,'']]],
+  ['core_2',['Modulo Core',['../md__2github_2workspace_2source_2modulo__core_2_r_e_a_d_m_e.html',1,'']]]
+];
---
+++ versions/v5.0.1/search/pages_2.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['modulo_0',['Modulo',['../index.html',1,'']]],
+  ['modulo_20components_1',['Modulo Components',['../md__2github_2workspace_2source_2modulo__components_2_r_e_a_d_m_e.html',1,'']]],
+  ['modulo_20controllers_2',['modulo-controllers',['../md__2github_2workspace_2source_2modulo__controllers_2_r_e_a_d_m_e.html',1,'']]],
+  ['modulo_20core_3',['Modulo Core',['../md__2github_2workspace_2source_2modulo__core_2_r_e_a_d_m_e.html',1,'']]],
+  ['modulo_20interfaces_4',['Modulo Interfaces',['../md__2github_2workspace_2source_2modulo__interfaces_2_r_e_a_d_m_e.html',1,'']]],
+  ['modulo_20utils_5',['Modulo Utils',['../md__2github_2workspace_2source_2modulo__utils_2_r_e_a_d_m_e.html',1,'']]]
+];
