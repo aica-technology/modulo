ceb78c0e15e9e93369454c7e3ea9171957485110
bpapaspyros <bpapaspyros@users.noreply.github.com>
2024-12-05 16:22:37 +0000

    deploy: 2723b073d70b095e4745428901b942c06ba4d272

--- versions/main/search/functions_7.js
+++ versions/main/search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['is_5factive_0',['is_active',['../classmodulo__controllers_1_1_base_controller_interface.html#a4ba640c6f8f7f61269af2cbdbd3ca0c9',1,'modulo_controllers::BaseControllerInterface']]]
+  ['is_5factive_0',['is_active',['../classmodulo__controllers_1_1_base_controller_interface.html#a4ba640c6f8f7f61269af2cbdbd3ca0c9',1,'modulo_controllers::BaseControllerInterface']]],
+  ['is_5fnode_5finitialized_1',['is_node_initialized',['../classmodulo__controllers_1_1_base_controller_interface.html#accd8315a77848aa5b4638f680106418e',1,'modulo_controllers::BaseControllerInterface']]]
 ];
