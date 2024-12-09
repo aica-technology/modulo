def40f4dc7b98f897fa4e196aea1ea3a379c4662
domire8 <domire8@users.noreply.github.com>
2024-12-09 14:50:47 +0000

    deploy: d8343e18dd205037c7644bb47335440fe3ac9799

--- versions/main/search/all_8.js
+++ versions/main/search/all_8.js
@@ -4,6 +4,5 @@ var searchData=
   ['interfaces_1',['Modulo Interfaces',['../md__2github_2workspace_2source_2modulo__interfaces_2_r_e_a_d_m_e.html',1,'']]],
   ['invalidpointercastexception_2',['InvalidPointerCastException',['../classmodulo__core_1_1exceptions_1_1_invalid_pointer_cast_exception.html',1,'modulo_core::exceptions']]],
   ['invalidpointerexception_3',['InvalidPointerException',['../classmodulo__core_1_1exceptions_1_1_invalid_pointer_exception.html',1,'modulo_core::exceptions']]],
-  ['is_5factive_4',['is_active',['../classmodulo__controllers_1_1_base_controller_interface.html#a4ba640c6f8f7f61269af2cbdbd3ca0c9',1,'modulo_controllers::BaseControllerInterface']]],
-  ['is_5fnode_5finitialized_5',['is_node_initialized',['../classmodulo__controllers_1_1_base_controller_interface.html#accd8315a77848aa5b4638f680106418e',1,'modulo_controllers::BaseControllerInterface']]]
+  ['is_5factive_4',['is_active',['../classmodulo__controllers_1_1_base_controller_interface.html#a4ba640c6f8f7f61269af2cbdbd3ca0c9',1,'modulo_controllers::BaseControllerInterface']]]
 ];
